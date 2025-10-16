d2d4a45d1aa3b51ab9956eed39646246900ae539
lyqgzbl <122811297+lyqgzbl@users.noreply.github.com>
2025-10-16 23:19:32 +0800

    fix: 更新部署分支名称为 pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 export default defineConfig({
 	site: "https://blog.lyqgzbl.com/",
 
-	base: "/blog",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
